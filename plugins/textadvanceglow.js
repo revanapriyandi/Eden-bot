39243915522f317c20e9ca4ee51865358be99dc4
revanapriyandi <revanapriyandi19@gmail.com>
2021-01-15 18:50:56 +0700

    fixed advanceglow

--- plugins/textadvanceglow.js
+++ plugins/textadvanceglow.js
@@ -3,7 +3,14 @@ let handler = async(m, { conn, text }) => {
 
     if (text > 10) return conn.reply(m.chat, '*Teks Terlalu Panjang!*\n_Maksimal 10 huruf!_', m)
 
-    let link = 'https://arugaz.my.id/api/textpro/advancedglow?text=' + text
+    new Promise((resolve, reject) => {
+        axios.get(`https://arugaz.my.id/api/textpro/advancedglow?text=` + text)
+            .then((res) => {
+                conn.sendFile(m.chat, res.data.result.link, 'image ', 'text advancedglow', m)
+
+            })
+            .catch(reject)
+    })
 
     conn.sendFile(m.chat, link, 'Nih bro')
 }
