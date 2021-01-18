77a66477fd8f490a94355fec7d91e0e43671e7d7
revanapriyandi <revanapriyandi19@gmail.com>
2021-01-18 23:08:19 +0700

    m

--- plugins/bug.js
+++ plugins/bug.js
@@ -4,7 +4,7 @@ let handler = async(m, { conn, text }) => {
     if (!text) return conn.reply(m.chat, 'Silahkan masukkan laporan', m)
     if (text > 300) return conn.reply(m.chat, 'Maaf Teks Terlalu Panjang, Maksimal 300 Teks', m)
     var nomor = m.sender
-    const teks1 = `*[REPORT]*\nNomor : @${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${text}`
+    const teks1 = `*[REPORT]*\nNomor : wa.me/${nomor.split("@s.whatsapp.net")[0]}\nPesan : ${text}`
     conn.sendMessage('6281261865875@s.whatsapp.net', teks1, MessageType.text)
     conn.reply(m.chat, 'Masalah telah di laporkan ke owner BOT, laporan palsu/main2 tidak akan ditanggapi.', m)
 }
