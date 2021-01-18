15c0893ffaf4aab5eb8a684501c34be56c52fefc
revanapriyandi <revanapriyandi19@gmail.com>
2021-01-18 23:03:14 +0700

    m

--- plugins/daily.js
+++ plugins/daily.js
@@ -1,7 +1,7 @@
 let handler = async(m, { conn }) => {
     if (new Date - global.DATABASE._data.users[m.sender].lastclaim > 86400000) {
         conn.reply(m.chat, '+500 XP', m)
-        global.DATABASE._data.users[m.sender].exp += 500
+        global.DATABASE._data.users[m.sender].exp += 50
         global.DATABASE._data.users[m.sender].lastclaim = new Date * 1
     } else conn.reply(m.chat, 'Anda sudah mengklaim klaim harian  hari ini', m)
 }
